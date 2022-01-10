2cc13cf2938b7fda4f2e2609ff2fff9f6e0283be
wagabooga <mattyhutwo@gmail.com>
2022-01-10 01:00:13 -0800

    got navbar

--- bucketlist/src/App.js
+++ bucketlist/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './App.css';
 import Navbar from "./Navbar";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
-   <>
+   <Router>
      <Navbar /> 
-   </>
+     <Switch>
+       <Route path="/" exact component={Home} />
+     </Switch>
+   </Router>
   )
 }
--- bucketlist/src/Navbar/index.js
+++ bucketlist/src/Navbar/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './NavbarElements'
 
 const Navbar = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Navbar</h1>
-    </div>
+    <>
+      <Nav>
+        <NavLink to="/">
+          <h1>Logo</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/experience" activeStyle>
+            Experience
+          </NavLink>
+          <NavLink to="/work" activeStyle>
+            Work
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            Contact
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
   )
 }
 
