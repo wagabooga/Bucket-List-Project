c636eccf19fbc2e8fd8a696d6a2aafe7bd5b660d
wagabooga <mattyhutwo@gmail.com>
2022-01-10 00:27:31 -0800

    Established navbar, installed React-router-dom, react-icons, and styled-components

---
+++ bucketlist/src/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
